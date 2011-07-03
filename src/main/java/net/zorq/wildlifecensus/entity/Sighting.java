84f64b1aa0df1049fcb8188d42eea56c0a791ebe
David Beaumont <david@zorq.net>
2011-07-03 12:59:16 +0100

    Add fields to Sighting

--- src/main/java/net/zorq/wildlifecensus/entity/Sighting.java
+++ src/main/java/net/zorq/wildlifecensus/entity/Sighting.java
@@ -1,5 +1,24 @@
 package net.zorq.wildlifecensus.entity;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+/**
+ * @author David Beaumont
+ */
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class Sighting {
 
+	private String description;
+	private double latitude;
+	private double longitude;
+	
+	public String getDescription() {return description;}
+	public void setDescription(String description) {this.description = description;}
+	
+	public double getLatitude() {return latitude;}
+	public void setLatitude(double latitude) {this.latitude = latitude;}
+	
+	public double getLongitude() {return longitude;}
+	public void setLongitude(double longitude) {this.longitude = longitude;}
+    
 }
