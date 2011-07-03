9816b19aa51220ffd7b7898855e14990f7c79d16
David Beaumont <david@zorq.net>
2011-07-03 13:12:58 +0100

    Get sightings from web site

--- src/main/java/net/zorq/wildlifecensus/service/Sightings.java
+++ src/main/java/net/zorq/wildlifecensus/service/Sightings.java
@@ -1,5 +1,20 @@
 package net.zorq.wildlifecensus.service;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import net.zorq.wildlifecensus.entity.Sighting;
+
+@Path("/sightings")
 public interface Sightings {
 
+	@GET
+	@Produces(APPLICATION_JSON)
+	public List<Sighting> getSightings();
+	
 }
--- src/test/java/net/zorq/wildlifecensus/AppTest.java
+++ src/test/java/net/zorq/wildlifecensus/AppTest.java
@@ -1,38 +1,29 @@
 package net.zorq.wildlifecensus;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.util.List;
+
+import net.zorq.wildlifecensus.entity.Sighting;
+import net.zorq.wildlifecensus.service.Sightings;
+
+import org.jboss.resteasy.client.ProxyFactory;
+import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * @author David Beaumont
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+	
+	private static final String URL_ROOT = "http://wildlifecensus.heroku.com/";
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void getSightings() {
+		RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
+		Sightings client = ProxyFactory.create(Sightings.class, URL_ROOT);
+		List<Sighting> output = client.getSightings();
+		Assert.assertFalse(output.isEmpty());
+	}
+	
 }
